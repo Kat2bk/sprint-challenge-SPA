44f52bc1def6e756bed35d44ea91489a8eb13851
unknown <Kat2bk@gmail.com>
2020-03-20 16:23:10 -0700

    installed dependencies, imported axios into Characterlist.js -- made axios call, and mapped over data

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,30 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [data, setData] = useState([]);
 
   useEffect(() => {
+    axios
+      .get("https://rick-api.herokuapp.com/api/")
+      .then(response => {
+        console.log("This is the axios call", response);
+        setData(response);
+      })
+      .catch(error => {
+        console.log("Unable to retrieve data", error);
+      });
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {data.map(item => (
+        <CharacterCard key={item.id} data={item} />
+      ))}
     </section>
   );
 }
