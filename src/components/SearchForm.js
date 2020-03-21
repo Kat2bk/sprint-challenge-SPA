ad68f9c73b12a8c4e1babf197f326c8ffab46a51
unknown <Kat2bk@gmail.com>
2020-03-20 18:46:54 -0700

    working on Search form more

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -2,22 +2,30 @@ import React, { useState, useEffect } from "react";
 
 export default function SearchForm({ items }) {
   const [searching, setSearching] = useState("");
-  console.log("this is the data", items);
   const handleChange = event => {
-    event.preventDefault();
     setSearching(event.target.value);
   };
 
-    const searchCharacters = items.filter(character =>
-      character.toLowerCase().includes(searching.toLowerCase())
-    );
+  // const searchCharacters = items.filter(character =>
+  //   character.toLowerCase().includes(searching.toLowerCase())
+  // );
+
+ function renderCharacters(characters) {
+    const search = this.state;
+    var data = characters.data.toLowerCase();
+
+    if (
+      search !== "" &&
+      items.name.toLowerCase().indexOf(search.toLOwerCase()) === -1
+    )
+      return null;
+  };
 
   return (
     <section className="search-form">
       <form>
         <label htmlFor="search">Search</label>
         <input id="search" type="text" name="search" onChange={handleChange} />
-        <button type="submit">Submit</button>
       </form>
     </section>
   );
